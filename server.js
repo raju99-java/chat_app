c53ea478a698a2c97d9650dae5e71e0b39a41087
raju99-java <rajudebnath.java@outlook.com>
2020-11-01 18:11:18 +0530

    nodejs project first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const { Socket } = require('dgram')
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, () =>{
+    console.log(`Listening on port ${PORT}`)
+})
+
+app.use(express.static('public',{ root: __dirname }))
+
+app.get('/',(req,res)=>{
+ res.sendFile('index.html',{ root: __dirname })
+})
+
+
+//Socket
+
+const io= require('socket.io')(http)
+
+io.on('connection',(socket) => {
+    console.log('Connected...')
+
+    socket.on('message',(msg) => {
+        socket.broadcast.emit('message',msg)
+    })
+})
